bff8b802c02e1399809663a2d71692d4b3a3fc9c
S_Joseph <ssyakmu@gmail.com>
2024-09-10 17:31:34 -0500

    Finish printing tnt and start symbol

--- First/first.cpp
+++ First/first.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstring>
 #include <string>
+#include <set>
 
 #include "first.h"
 
@@ -27,12 +28,21 @@ Production::Production() {
 }
 
 //******************************************************************************
+Production::Production(char non_terminal) {
+    lhs = non_terminal;
+    rhs = nullptr;
+    nextProduction = nullptr;
+}
+
+//******************************************************************************
 Grammar::Grammar() {
     startProduction = nullptr;
     startSymbol = 0;
 }
 
 //******************************************************************************
+
+// Function to read grammar from file
 int Grammar::readGrammar(string& filename) {
     int rc = 0;
 
@@ -42,11 +52,11 @@ int Grammar::readGrammar(string& filename) {
     // Check if the file is open, if not print an error message
     if (!file.is_open()) {
         cout << "File not found" << endl;
-        rc = -1;
+        return -1;
     }
 
     string line;
-    while(getline(file,line)){
+    while (getline(file, line)) {
         if (line.empty()) {
             continue;
         }
@@ -54,51 +64,51 @@ int Grammar::readGrammar(string& filename) {
         // The first symbol shall be the non-terminal in left hand side
         char lhs = line[0];
 
-        // Save the first symbol as the start symbol
-        if(startProduction == nullptr) {
+        // Save the first symbol as the start symbol if this is the first production
+        if (startProduction == nullptr) {
             startSymbol = lhs;
         }
 
         // Create a new production
         Production *newProduction = new Production(lhs);
-        if(startProduction == nullptr) {
+        if (startProduction == nullptr) {
             startProduction = newProduction;
         } else {
-            // If there are already productions, add the new production to the end
+            // Add the new production to the end of the list
             Production *current = startProduction;
-            while(current->nextProduction != nullptr) {
+            while (current->nextProduction != nullptr) {
                 current = current->nextProduction;
             }
             current->nextProduction = newProduction;
         }
 
-        // Create a new node for the first symbol in the right hand side
-        node* prev = nullptr;
-        for(int i = 4; i < line.length(); i++) {
+        // Create nodes for the right-hand side of the production
+        node *prev = nullptr;
+        for (int i = 4; i < line.length(); i++) {
             char sym = line[i];
-            node* newNode = new node(sym);
-            if(prev == nullptr) {
+            node *newNode = new node(sym);
+            if (prev == nullptr) {
                 newProduction->rhs = newNode;
             } else {
                 prev->next = newNode;
             }
             prev = newNode;
         }
-
     }
 
     file.close();
-
     return rc;
 }
 
 //******************************************************************************
+
+// Function to print the grammar
 void Grammar::printGrammar() {
     Production *current = startProduction;
-    while(current != nullptr) {
+    while (current != nullptr) {
         cout << current->lhs << " -> ";
         node *currentNode = current->rhs;
-        while(currentNode != nullptr) {
+        while (currentNode != nullptr) {
             cout << currentNode->symbol;
             currentNode = currentNode->next;
         }
@@ -108,13 +118,87 @@ void Grammar::printGrammar() {
 }
 
 //******************************************************************************
+
+// Helper function to check if a symbol is a non-terminal
+bool Grammar::isNonTerminal(char symbol) {
+    return symbol >= 'A' && symbol <= 'Z';
+}
+
+//******************************************************************************
+
+// Helper function to check if a symbol is a terminal
+bool Grammar::isTerminal(char symbol) {
+    return symbol >= 'a' && symbol <= 'z';
+}
+
+//******************************************************************************
+
+// Function to print all terminals
+void Grammar::printTerminals() {
+    set<char> terminals;
+    Production *current = startProduction;
+    while (current != nullptr) {
+        node *currentNode = current->rhs;
+        while (currentNode != nullptr) {
+            char symbol = currentNode->symbol;
+            if (isTerminal(symbol)) {
+                terminals.insert(symbol);
+            }
+            currentNode = currentNode->next;
+        }
+        current = current->nextProduction;
+    }
+
+    cout << "Terminals: ";
+    for (char terminal : terminals) {
+        cout << terminal << " ";
+    }
+    cout << endl;
+}
+
+//******************************************************************************
+
+// Function to print all non-terminals
+void Grammar::printNonTerminals() {
+    set<char> nonTerminals;
+    Production *current = startProduction;
+    while (current != nullptr) {
+        nonTerminals.insert(current->lhs);
+        node *currentNode = current->rhs;
+        while (currentNode != nullptr) {
+            char symbol = currentNode->symbol;
+            if (isNonTerminal(symbol)) {
+                nonTerminals.insert(symbol);
+            }
+            currentNode = currentNode->next;
+        }
+        current = current->nextProduction;
+    }
+
+    cout << "Non-terminals: ";
+    for (char nonTerminal : nonTerminals) {
+        cout << nonTerminal << " ";
+    }
+    cout << endl;
+}
+
+//******************************************************************************
+// Function to print the start symbol
+void Grammar::printStartSymbol() {
+    cout << "Start symbol: " << startSymbol << endl;
+}
+
+//******************************************************************************
+// Main function
 int main() {
     Grammar grammar;
     string filename = "grammar.txt";
     int rc = grammar.readGrammar(filename);
-    if(rc == 0) {
+    if (rc == 0) {
         grammar.printGrammar();
+        grammar.printTerminals();
+        grammar.printNonTerminals();
+        grammar.printStartSymbol();
     }
-
     return 0;
 }
--- First/first.h
+++ First/first.h
@@ -1,45 +1,52 @@
 #ifndef FIRST_H
 #define FIRST_H
 
+#include <string>
+#include <set>
+
 using namespace std;
 
 class node {
     private:
         char symbol;
-        // Pointer to the next node
         node *next;
+        node *nextNT;
     
     public:
         node();
         node(char sym);
+
         friend class Grammar;
 };
 
-class Production{
+class Production {
     private:
         char lhs;
-        // Start node of right hand side of the production
         node *rhs;
-        // Pointer to the next production
         Production *nextProduction;
     
     public:
         Production();
-        Production(char non_terminal) : lhs(non_terminal), rhs(nullptr), nextProduction(nullptr) {};
+        Production(char non_terminal);
 
         friend class Grammar;
 };
 
-// Structure to store the grammar
 class Grammar {
     private:
         Production *startProduction;
         char startSymbol;
 
+        bool isNonTerminal(char symbol);  // Helper function to identify non-terminals
+        bool isTerminal(char symbol);
+
     public:
         Grammar();
         int readGrammar(string& filename);
         void printGrammar();
+        void printTerminals();
+        void printNonTerminals();
+        void printStartSymbol();
 };
 
 #endif
