f0c18c9fa06bdd944a2ae5f954b3487b10227213
S_Joseph <ssyakmu@gmail.com>
2024-09-10 05:37:41 -0500

    Test version of Read/Print Grammar

--- First/first.cpp
+++ First/first.cpp
@@ -4,40 +4,93 @@
 #include <string>
 
 #include "first.h"
-#include "grammar.txt"
 
-#define MAX 10
 
 using namespace std;
 
-// Structure to store the grammar
-Grammar::Grammar() {
-    int numProductions = 0;
-};
+//******************************************************************************
+int Grammar::readGrammar(string& filename) {
+    int rc = 0;
 
-// Function to calculate FIRST
-void Grammar::calculateFirst(char c) {
-    int i, j;
-    // If the symbol is lowercase(non-terminal), then add it to the FIRST set
-    if(!(isupper(c))) {
-        first = c;
-    }
-}
-
-void Grammar::readGrammar(const string& filename) {
+    // Open the file
     ifstream file(filename);
+
+    // Check if the file is open, if not print an error message
+    if (!file.is_open()) {
+        cout << "File not found" << endl;
+        rc = -1;
+    }
+
     string line;
-    while (getline(file, line)) {
-        strcpy(production[numProductions++], line.c_str());
+    while(getline(file,line)){
+        if (line.empty()) {
+            continue;
+        }
+
+        // The first symbol shall be the non-terminal in left hand side
+        char lhs = line[0];
+
+        // Save the first symbol as the start symbol
+        if(startProduction == nullptr) {
+            startSymbol = lhs;
+        }
+
+        // Create a new production
+        Production *newProduction = new Production(lhs);
+        if(startProduction == nullptr) {
+            startProduction = newProduction;
+        } else {
+            // If there are already productions, add the new production to the end
+            Production *current = startProduction;
+            while(current->nextProduction != nullptr) {
+                current = current->nextProduction;
+            }
+            current->nextProduction = newProduction;
+        }
+
+        // Create a new node for the first symbol in the right hand side
+        node* prev = nullptr;
+        for(int i = 2; i < line.length(); i++) {
+            char sym = line[i];
+            node* newNode = new node(sym);
+            if(prev == nullptr) {
+                newProduction->rhs = newNode;
+            } else {
+                prev->next = newNode;
+            }
+            prev = newNode;
+        }
+
+    }
+
+    file.close();
+
+    return rc;
+}
+
+//******************************************************************************
+void Grammar::printGrammar() {
+    Production *current = startProduction;
+    while(current != nullptr) {
+        cout << current->lhs << " -> ";
+        node *currentNode = current->rhs;
+        while(currentNode != nullptr) {
+            cout << currentNode->symbol;
+            currentNode = currentNode->next;
+        }
+        cout << endl;
+        current = current->nextProduction;
     }
 }
 
+//******************************************************************************
 int main() {
-    Grammar g;
-    g.readGrammar("grammar.txt");
-
-    char c;
-
+    Grammar grammar;
+    string filename = "grammar.txt";
+    int rc = grammar.readGrammar(filename);
+    if(rc == 0) {
+        grammar.printGrammar();
+    }
 
     return 0;
 }
--- First/first.h
+++ First/first.h
@@ -1,19 +1,46 @@
 #ifndef FIRST_H
 #define FIRST_H
 
-#define MAX 10
 using namespace std;
 
+class node {
+    private:
+        char symbol;
+        // Pointer to the next node
+        node *next;
+        // Pointer to the next non-terminal
+        node *nextNT;
+    
+    public:
+        node(char sym) : symbol(sym), next(nullptr), nextNT(nullptr) {}
+
+        friend class Grammar;
+};
+
+class Production{
+    private:
+        char lhs;
+        // Start node of right hand side of the production
+        node *rhs;
+        // Pointer to the next production
+        Production *nextProduction;
+    
+    public:
+        Production(char non_terminal) : lhs(non_terminal), rhs(nullptr), nextProduction(nullptr) {};
+
+        friend class Grammar;
+};
 
 // Structure to store the grammar
 class Grammar {
     private:
-        char production[MAX][MAX];
-        char first;
-        int numProductions;
+        Production *startProduction;
+        char startSymbol;
+
     public:
-        void calculateFirst(char c);
-        void readGrammar(const string& filename);
+        Grammar() : startProduction(nullptr), startSymbol(0) {};
+        int readGrammar(string& filename);
+        void printGrammar();
 };
 
 #endif
